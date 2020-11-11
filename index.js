67f6ee4fc4b85a5f42797b3c4e9d55f82b0cd8ea
Vaibhav10sept <vaibhavtiwari10sept@gmail.com>
2020-11-11 20:18:20 +0530

    generating token and login routing

--- index.js
+++ index.js
@@ -3,16 +3,14 @@ const app = express();
 const bodyParser = require("body-parser");
 const { User } = require("./models/user");
 const mongoose = require("mongoose");
-const config = require("./config/key")
+const config = require("./config/key");
 
 const cookieParser = require("cookie-parser");
 const user = require("./models/user");
 mongoose
-  .connect(
-config.mongoURI,   {
-      useNewUrlParser: true,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+  })
   .then(() => {
     console.log("DB connected").catch((err) => console.log(err));
   });
@@ -26,7 +24,34 @@ app.post("/api/users/register", (res, req) => {
     if (err) return res.json({ success: false, err });
     return res.statusCode(200).json({
       success: true,
-      userData: doc
+      userData: doc,
+    });
+  });
+});
+
+app.post("/api/user/login", (res, req) => {
+  // find the email
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user)
+      return res.json({
+        loginsuccess: false,
+        message: "auth failed, email not found ",
+      });
+    // compare the pasword
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginsuccess: false,
+          message: "wrong password",
+        });
+      // generate token
+
+      user.generateToken((err, user) => {
+          if(err) return res.statusCode(400).send(err)
+          res.cookie('x_auth',user.token).status(200).json({
+              loginsuccess:true
+          })
+      });
     });
   });
 });
